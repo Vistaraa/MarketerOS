620b11e5ca8ceaf4ba94c752a8d3657b8ffea9f0
Heet369 <king963patel@gmail.com>
2026-09-20 14:47:08 +0530

    Initialize Prisma client with global instance caching

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,4 +6,4 @@ const globalForPrisma = globalThis as unknown as { marketerPrisma?: PrismaClient
 
 export const prisma = globalForPrisma.marketerPrisma ?? new PrismaClient({ log: ["error"] });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.marketerPrisma = prisma;
+globalForPrisma.marketerPrisma = prisma;
